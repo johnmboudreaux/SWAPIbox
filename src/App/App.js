4956b6e54428cce2f458974426580a57000d058d
Nik Bornemeier <NikolausRB@gmail.com>
2017-10-10 15:48:16 -0600

    create single fetch call for list

--- src/App/App.js
+++ src/App/App.js
@@ -11,6 +11,44 @@ class App extends Component {
     };
   }
 
+  cleanVehicleData(arrayOfVehicles) {
+    const rawArray = arrayOfVehicles;
+    console.log(rawArray)
+    const cleanArray = rawArray.map(({ name, model, passengers, vehicle_class }) => {
+      const newVehicle = {
+         name,
+         model,
+         vehicle_class,
+         passengers
+      }
+      return newVehicle;
+    })
+    console.log(cleanArray)
+  }
+
+  fetchList(type) {
+    return fetch(`https://swapi.co/api/${type}/`)
+      .then(returnedData => returnedData.json())
+      .then(returnedData => returnedData.results.map(personPlaceOrThing => {
+        if(type === 'people'){
+          fetch(personPlaceOrThing.homeworld)
+            .then(response => response.json())
+            .then(homeWorld => personPlaceOrThing.homeworld = homeWorld);
+        }
+        if (type === 'planets') {
+          const allResidents = personPlaceOrThing.residents.map(residentAPI => {
+            return fetch(residentAPI)
+              .then(response => response.json());
+          });
+          
+          Promise.all(allResidents)
+            .then(residents => personPlaceOrThing.residents = residents);
+        }
+        return personPlaceOrThing;
+      }))
+      .then(data => console.log(data));
+  }
+
   fetchPeople() {
     return fetch(`https://swapi.co/api/people/`)
       .then(returnedData => returnedData.json())
@@ -20,7 +58,7 @@ class App extends Component {
           .then(homeWorld => person.homeworld = homeWorld);
         return person;
       }))
-      .then(data => console.log(data));
+      // .then(data => console.log(data));
   }
 
   fetchVehicles() {
@@ -29,7 +67,8 @@ class App extends Component {
       .then(vehicles => vehicles.results.map(vehicle => {
         return vehicle;
       }))
-      .then(response => console.log(response))
+      .then(response => this.cleanVehicleData(response))
+      .then()
       // .then(vehicles => vehicles.map((vehicle) => {
       //   return Object.assign({}, vehicle);
       // }));
@@ -38,8 +77,9 @@ class App extends Component {
   componentDidMount() {
     // const helper = new Helper();
     // helper.getData('people')
-    this.fetchPeople();
-    this.fetchVehicles();
+    this.fetchList('planets');
+    // this.fetchPeople();
+    // this.fetchVehicles();
     // this.setState({
     //   people: helper.getData('people')
     // });
