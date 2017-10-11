e3e5305028a12b7e68674fdb56b40d3fc1b53179
Alex Banister <alexbanister@gmail.com>
2017-10-10 20:49:35 -0600

    add header styles

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../Button/Button';
-
+import logo from '../assets/logo.svg'
 
 const Header = () => {
   return (
-    <div>
-      <h1><a href="/">SWAPI-Box</a></h1>
-      <Button path='/people' text='People' />
-      <Button path='/planets' text='Planets' />
-      <Button path='/vehicles' text='Vehicles' />
-      <Button path='/favorites' text='Favorites' />
+    <div className='header'>
+      <a href="/"><img src={logo} alt='logo' /></a>
+      <nav>
+        <Button path='/people' text='People' />
+        <Button path='/planets' text='Planets' />
+        <Button path='/vehicles' text='Vehicles' />
+        <Button path='/favorites' text='Favorites' />
+      </nav>
     </div>
   );
 };
